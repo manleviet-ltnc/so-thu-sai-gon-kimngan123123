b62614bf2f7ede545d34a913e3bf8d14d61be8b6
Đặng Nguyên Kim Ngân <31848168+kimngan123123@users.noreply.github.com>
2017-10-02 18:44:20 +0700

    Sở thú
    Commit Sở Thú

---
+++ SothuXiGon/SothuXiGon/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SothuXiGon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstThuMoi = new System.Windows.Forms.ListBox();
+            this.lstDanhSach = new System.Windows.Forms.ListBox();
+            this.btnChon = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thú mới";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh sách thú";
+            // 
+            // lstThuMoi
+            // 
+            this.lstThuMoi.AllowDrop = true;
+            this.lstThuMoi.FormattingEnabled = true;
+            this.lstThuMoi.Items.AddRange(new object[] {
+            "Chó",
+            "Mèo",
+            "Khỉ",
+            "Sư Tử",
+            "Báo",
+            "Trăn"});
+            this.lstThuMoi.Location = new System.Drawing.Point(12, 29);
+            this.lstThuMoi.Name = "lstThuMoi";
+            this.lstThuMoi.Size = new System.Drawing.Size(128, 134);
+            this.lstThuMoi.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.lstThuMoi, "Ấn chuột và kéo sang Danh sách thú để tạo Sở thú");
+            this.lstThuMoi.DragEnter += new System.Windows.Forms.DragEventHandler(this.Listbox_DragEnter);
+            this.lstThuMoi.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
+            // 
+            // lstDanhSach
+            // 
+            this.lstDanhSach.AllowDrop = true;
+            this.lstDanhSach.FormattingEnabled = true;
+            this.lstDanhSach.Location = new System.Drawing.Point(228, 29);
+            this.lstDanhSach.Name = "lstDanhSach";
+            this.lstDanhSach.Size = new System.Drawing.Size(128, 134);
+            this.lstDanhSach.TabIndex = 3;
+            this.lstDanhSach.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstDanhSach_DragDrop);
+            this.lstDanhSach.DragEnter += new System.Windows.Forms.DragEventHandler(this.Listbox_DragEnter);
+            this.lstDanhSach.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
+            // 
+            // btnChon
+            // 
+            this.btnChon.Location = new System.Drawing.Point(148, 81);
+            this.btnChon.Name = "btnChon";
+            this.btnChon.Size = new System.Drawing.Size(75, 23);
+            this.btnChon.TabIndex = 4;
+            this.btnChon.Text = "Chon";
+            this.btnChon.UseVisualStyleBackColor = true;
+            this.btnChon.Click += new System.EventHandler(this.btnChon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 185);
+            this.Controls.Add(this.btnChon);
+            this.Controls.Add(this.lstDanhSach);
+            this.Controls.Add(this.lstThuMoi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sở Thú Xì Gòn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstThuMoi;
+        private System.Windows.Forms.ListBox lstDanhSach;
+        private System.Windows.Forms.Button btnChon;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ SothuXiGon/SothuXiGon/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SothuXiGon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            lstDanhSach.Items.Add(lstThuMoi.SelectedItem);
+        }
+
+        private void ListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            int index = lb.IndexFromPoint(e.X, e.Y);
+
+            lb.DoDragDrop(lb.Items[index].ToString(),
+                          DragDropEffects.Copy);
+        }
+
+        private void Listbox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void lstDanhSach_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                ListBox lb = (ListBox)sender;
+                lb.Items.Add(e.Data.GetData(DataFormats.Text));
+            }
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            //Mở tệp tin
+            StreamWriter writer = new StreamWriter("danhsachthu.txt");
+
+            if (writer == null) return;
+
+            foreach (var item in lstDanhSach.Items)
+                writer.WriteLine(item.ToString());
+
+            writer.Close();
+        }
+    }
+}
